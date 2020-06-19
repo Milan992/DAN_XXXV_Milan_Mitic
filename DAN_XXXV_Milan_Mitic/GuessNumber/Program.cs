04f5e54699fdebb398c5e9cba167a949e5388bda
Milan992 <mitic.gsm@gmail.com>
2020-06-19 11:49:12 +0200

    Instert number method

--- DAN_XXXV_Milan_Mitic/GuessNumber/Program.cs
+++ DAN_XXXV_Milan_Mitic/GuessNumber/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GuessNumber
@@ -10,6 +11,29 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
+            Thread begin = new Thread(() => InsertParticipantAmount());
+        }
+
+        private static void InsertParticipantAmount()
+        {
+            string participants = "";
+            uint numberOfParticipants;
+
+            while (!uint.TryParse(participants, out numberOfParticipants))
+            {
+                Console.WriteLine("Please enter the number of participants:");
+                participants = Console.ReadLine();
+            }
+
+            string numberToGuess = "";
+            int number;
+
+            while (!int.TryParse(numberToGuess, out number) && number > 100 && number < 1)
+            {
+                Console.WriteLine("Please enter a number to guess. The number muust be 1 - 100.");
+                numberToGuess = Console.ReadLine();
+            }
+
         }
     }
 }
